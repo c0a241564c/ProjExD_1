6f0a14798893fce8bf5bb5a03652cf7a30eb5984
“c0a24156” <c0a241564c@edu.teu.ac.jp>
2025-06-10 16:28:45 +0900

    演習Ⅰ押していない場合

--- pygame_image.py
+++ pygame_image.py
@@ -20,16 +20,18 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-            Key_lst=pg.key.get_pressed()
-            if Key_lst[pg.K_UP]:
-                kk_rct.move_ip(0,-1)
-            if Key_lst[pg.K_DOWN]:
-                kk_rct.move_ip(0,+1)
-            if Key_lst[pg.K_LEFT]:
-                kk_rct.move_ip(-1,0)
-            if Key_lst[pg.K_RIGHT]:
-                kk_rct.move_ip(+1,0)
-            x=tmr%3200
+        Key_lst=pg.key.get_pressed()
+        if Key_lst[pg.K_UP]:
+             kk_rct.move_ip(0,-1)
+        if Key_lst[pg.K_DOWN]:
+             kk_rct.move_ip(0,+1)
+        if Key_lst[pg.K_LEFT]:
+             kk_rct.move_ip(-1,0)
+        if Key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(0,0)
+        else:
+            kk_rct.move_ip(-1,0)
+        x=tmr%3200
         screen.blit(bg_img, [-x, 0]) #一枚目
         screen.blit(bg_img2, [-x+1600, 0]) #二枚目
         screen.blit(bg_img, [-x+3200, 0]) #三枚目
