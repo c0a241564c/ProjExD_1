9a6922148b1f286f24e00145ec4b10b5445a525f
“c0a24156” <c0a241564c@edu.teu.ac.jp>
2025-06-10 15:29:53 +0900

    こうかとん追加

--- pygame_image.py
+++ pygame_image.py
@@ -9,13 +9,16 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg") #Surface
+    kouka=pg.image.load("fig/3.png")
+    kouka=pg.transform.flip(kouka, True ,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kouka, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
